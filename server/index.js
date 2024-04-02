57b3ae7bb5b0102f984a69ed8c8051c9977575bd
D5CD1198KDF <akshaydhayalkd99@gmail.com>
2024-04-02 06:11:38 +0530

    joined client to the server code

--- server/index.js
+++ server/index.js
@@ -6,36 +6,37 @@ import messageRouter from "./routes/message.routes.js";
 import userRouter from "./routes/user.routes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import {app, server } from "./socket/socket.js";
+import { app, server } from "./socket/socket.js";
+import path from "path";
 
 dotenv.config();
 
+const __dirname = path.resolve();
 // const app=express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-app.use("/api/auth",authRouter);
-app.use("/api/messages",messageRouter);
-app.use("/api/users",userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/messages", messageRouter);
+app.use("/api/users", userRouter);
 
-const PORT=process.env.PORT || 3000;
-server.listen(PORT,()=>{
-    dbConnect();
-    console.log("server ia running at port "+PORT);
-})
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/dist/index.html"));
+});
 
-
-
-
-
-
-
-
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  dbConnect();
+  console.log("server ia running at port " + PORT);
+});
 
 // const app=express();
 // app.use(express.json());
